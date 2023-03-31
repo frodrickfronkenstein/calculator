0c10d3aa70224177069df228c41f1acd6fffdda4
eric Smith <ericandrewsmith88@gmail.com>
2023-03-31 10:09:46 -0400

    simplified numpad

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,24 +7,15 @@ var graphicEl = document.querySelector("#graphic");
 var outputEl = document.querySelector("#output");
 
 //buttons
+var numPadEl = document.querySelectorAll(".zero-nine");
+var decimalEl = document.querySelector("#decimal");
 var acEl = document.querySelector("#ac");
 var plusMinusEl = document.querySelector("#plus-minus");
 var percentageEl = document.querySelector("#percentage");
 var divideEl = document.querySelector("#divide");
-var sevenEl = document.querySelector("#seven");
-var eightEl = document.querySelector("#eight");
-var nineEl = document.querySelector("#nine");
 var multiplyEl = document.querySelector("#multiply");
-var fourEl = document.querySelector("#four");
-var fiveEl = document.querySelector("#five");
-var sixEl = document.querySelector("#six");
 var minusEl = document.querySelector("#minus");
-var oneEl = document.querySelector("#one");
-var twoEl = document.querySelector("#two");
-var threeEl = document.querySelector("#three");
 var plusEl = document.querySelector("#plus");
-var zeroEl = document.querySelector("#zero");
-var decimalEl = document.querySelector("#decimal");
 var equalEl = document.querySelector("#equal");
 
 //choose calculator
@@ -46,19 +37,16 @@ var pushNumber = function() {
         console.log("overflow")
     } else {
         a.push(i);
-        console.log(a);
     }
 };
 
 //display number
 var displayNumber = function() {
     var x = a.join('');
-    x = x.replace(/^0+/, '');
     if (x === "") {
         x = "0";
     };
     outputEl.textContent = x;
-    console.log(a);
 }
 //get rid of 0 at beginning of array
 var ifZero = function() {
@@ -69,107 +57,32 @@ var ifZero = function() {
     }
 }
 
-// number buttons
-zeroEl.addEventListener('click', function handleClick(event) {
-    event
-    i="0";
-    ifZero();
-    pushNumber();
-    displayNumber();
-    console.log(i);
+// numpad
+numPadEl.forEach(numPad => {
+    numPad.addEventListener('click', function handleClick(event) {
+        event
+        i = numPad.innerHTML;
+        console.log(i);
+        ifZero();
+        pushNumber();
+        displayNumber();
+    })
 })
-oneEl.addEventListener('click', function handleClick(event) {
-    event
-    i="1";
-    ifZero();
-    pushNumber();
-    displayNumber();
-    console.log(i);
-})
-twoEl.addEventListener('click', function handleClick(event) {
-    event
-    i="2";
-    ifZero();
-    pushNumber();
-    displayNumber();
-    console.log(i);
-})
-threeEl.addEventListener('click', function handleClick(event) {
-    event
-    i="3";
-    ifZero();
-    pushNumber();
-    displayNumber();
-    console.log(i);
-})
-fourEl.addEventListener('click', function handleClick(event) {
-    event
-    i="4";
-    ifZero();
-    pushNumber();
-    displayNumber();
-    console.log(i);
 
-})
-fiveEl.addEventListener('click', function handleClick(event) {
-    event
-    i="5";
-    ifZero();
-    pushNumber();
-    displayNumber();
-    console.log(i);
-})
-sixEl.addEventListener('click', function handleClick(event) {
-    event
-    i="6";
-    ifZero();
-    pushNumber();
-    displayNumber();
-    console.log(i);
-})
-sevenEl.addEventListener('click', function handleClick(event) {
-    event
-    i="7";
-    ifZero();
-    pushNumber();
-    displayNumber();
-    console.log(i);
-})
-eightEl.addEventListener('click', function handleClick(event) {
-    event
-    i="8";
-    ifZero();
-    pushNumber();
-    displayNumber();
-    console.log(i)
-})
-nineEl.addEventListener('click', function handleClick(event) {
-    event
-    i="9";
-    ifZero();
-    pushNumber();
-    displayNumber();
-    console.log(i)
-})
 decimalEl.addEventListener('click', function handleClick(event) {
     event
     i=".";
-    if (a.includes(".")) {
-        console.log(i);
-    } else {
+    if (!a.includes(".")) {
         ifZero();
         pushNumber();
         var x = a.join('');
         outputEl.textContent = x;
-        console.log(a);
-        console.log(i)
     }
 })
 
 // operator buttons
 divideEl.addEventListener('click', function handleClick(event) {
     event
-    console.log("divide");
     var x = a.join('');
     b.push(x);
     if (b.includes("/") || b.includes("*") || b.includes("+") || b.includes("-")) {
@@ -178,11 +91,9 @@ divideEl.addEventListener('click', function handleClick(event) {
         b.push("/");
     }
     a = [];
-    console.log(b);
 })
 multiplyEl.addEventListener('click', function handleClick(event) {
     event
-    console.log("multiply");
     var x = a.join('');
     b.push(x);
     if (b.includes("/") || b.includes("*") || b.includes("+") || b.includes("-")) {
@@ -191,11 +102,9 @@ multiplyEl.addEventListener('click', function handleClick(event) {
         b.push("*");
     }
     a = [];
-    console.log(b);
 })
 minusEl.addEventListener('click', function handleClick(event) {
     event
-    console.log("minus");
     var x = a.join('');
     b.push(x);
     if (b.includes("/") || b.includes("*") || b.includes("+") || b.includes("-")) {
@@ -204,11 +113,9 @@ minusEl.addEventListener('click', function handleClick(event) {
         b.push("-");
     }
     a = [];
-    console.log(b);
 })
 plusEl.addEventListener('click', function handleClick(event) {
     event
-    console.log("plus");
     var x = a.join('');
     b.push(x);
     if (b.includes("/") || b.includes("*") || b.includes("+") || b.includes("-")) {
@@ -217,11 +124,9 @@ plusEl.addEventListener('click', function handleClick(event) {
         b.push("+");
     }
     a = [];
-    console.log(b);
 })
 equalEl.addEventListener('click', function handleClick(event) {
     event
-    console.log("equal")
     var x = a.join('');
     b.push(x);
     var y = b.join('');
@@ -246,11 +151,9 @@ plusMinusEl.addEventListener('click', function handleClick(event) {
         a.unshift("-");
     }
     displayNumber();
-    console.log("plus-minus")
 })
 percentageEl.addEventListener('click', function handleClick(event) {
     event
-    console.log("percentage")
     var x = a.join('');
     a = ["0"];
     var x = x * 0.01;
